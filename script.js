b8c47f9fd4cd0f8b520f9b67520dce6022ae4713
ndallen <ecommerceevolution@gmail.com>
2019-03-09 14:39:24 +0800

    function complete

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 const friends = ['Rupert', 'Norman', 'Stanley', 'Cecil', 'Colin'];
 
+const song = () => {
 
+for(let i = 0; i < friends.length; i++){
+  for(let n = 100; n > 0 ; n-- ) {
+    if(n > 1) {
+      console.log(`${n} bottles of beer on the wall, ${n} bottles of beer, ${friends[i]} drinks one down, passes it around , ${n - 1} bottles of beer on the wall`)
+    } else {
+      console.log(`1 bottle of beer on the wall, 1 bottle of beer, ${friends[i]} drinks one down, passes it around , no more bottles of beer on the wall`)
+    }
+    }
+  }
+}
+song();
